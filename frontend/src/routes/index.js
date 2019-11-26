b7116eeb0b131520f47b2cc94e37825b391930dd
Guilherme Silva <guisilva.master@gmail.com>
2019-11-26 15:00:16 -0300

    Fix router files issues

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from '../pages/login';
-import SignUp from '../pages/signun';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+// import SignUp from '../pages/SignUp';
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Login} />
-    {/* <Route path="/playlists/:id" component={SignUp} /> */}
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      {/* <Route path="/playlists/:id" component={SignUp} /> */}
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
