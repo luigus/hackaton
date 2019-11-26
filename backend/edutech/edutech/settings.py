25e03d9fc4e639a1e71c33e6e418e4e3de07f7e3
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2019-11-26 14:25:23 -0300

    configurando para usar mysql

--- backend/edutech/edutech/settings.py
+++ backend/edutech/edutech/settings.py
@@ -46,10 +46,13 @@ WSGI_APPLICATION = 'edutech.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edutec',
+        'USER': 'django', 
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost'
+    } 
 }
 
 
