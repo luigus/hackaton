8f5c79f829f3c0c588d545701090356711f5aad0
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2019-11-27 12:02:42 -0300

    adicionando relacionento das entidades

--- backend/edutech/student/serializers.py
+++ backend/edutech/student/serializers.py
@@ -1,8 +1,54 @@
 from rest_framework import serializers
-from .models import Aluno
+from .models import Aluno, Endereco, Cidade, Escola
+
+class CidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cidade
+
+        fields = (
+            'nome',
+            'estado',
+        )
+
+class EnderecoSerializer(serializers.ModelSerializer):
+    cidade = CidadeSerializer(required=True)
+    
+    class Meta:
+        model = Endereco
+
+        fields = (
+            'logradouro',
+            'numero',
+            'cidade' ,
+            'estado'
+        )
+
+
+class EscolaSerializer(serializers.ModelSerializer):
+    endereco = EnderecoSerializer(required=True)
+
+    class Meta:
+        model = Escola
+
+        fields = (
+            'nome',
+            'endereco',
+        )
 
 class AlunoSerializer(serializers.ModelSerializer):
 
+    endereco = EnderecoSerializer(required=True)
+    escola = EscolaSerializer(required=True)
+
     class Meta:
         model = Aluno
-        fields = '__all__'
+
+        fields = (
+            'nome',
+            'nascimento',
+            'escola',
+            'serie',
+            'email',
+            'senha',
+            'endereco'
+        )
