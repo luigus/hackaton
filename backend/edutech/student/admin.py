e38d58b1235ae2d3e919e83ec71a2bde082ea4bb
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2019-11-27 11:05:03 -0300

    ajuste na modelagem

--- backend/edutech/student/admin.py
+++ backend/edutech/student/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from    .models import Aluno, Endereco, Cidade, Estado, Escola
+from    .models import Aluno, Endereco, Cidade, Escola
 
 admin.site.register(Aluno)
 admin.site.register(Endereco)
 admin.site.register(Cidade)
-admin.site.register(Estado)
 admin.site.register(Escola)
--- backend/edutech/student/models.py
+++ backend/edutech/student/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
-class Estado (models.Model):
-    nome = models.CharField(max_length = 200)
-
-    def __str__(self):
-        return self.nome
+UF_CHOICES = (
+    ('AC', 'Acre'), 
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranão'),
+    ('MG', 'Minas Gerais'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MT', 'Mato Grosso'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PE', 'Pernanbuco'),
+    ('PI', 'Piauí'),
+    ('PR', 'Paraná'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('RS', 'Rio Grande do Sul'),
+    ('SC', 'Santa Catarina'),
+    ('SE', 'Sergipe'),
+    ('SP', 'São Paulo'),
+    ('TO', 'Tocantins')
+)
 
 class Cidade (models.Model):
     nome = models.CharField(max_length = 200)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    estado = models.CharField(max_length = 200, choices = UF_CHOICES )
 
     def __str__(self):
         return self.nome
@@ -17,7 +40,7 @@ class Endereco (models.Model):
     logradouro = models.CharField(max_length = 200)
     numero = models.IntegerField()
     cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    estado = models.CharField(max_length = 200, choices = UF_CHOICES )
 
     def __str__(self):
         return self.logradouro
@@ -30,7 +53,7 @@ class Escola (models.Model):
         return self.nome
 
 class Aluno (models.Model):
-    series = [
+    SERIE_CHOICES = [
         ('1', 'primeiro ano'),
         ('2', 'segundo ano'),
         ('3', 'terceiro ano')
@@ -39,7 +62,7 @@ class Aluno (models.Model):
     nome = models.CharField(max_length = 200)
     nascimento = models.DateField()
     escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
-    serie = models.CharField(max_length = 30, choices=series, default=1)
+    serie = models.CharField(max_length = 30, choices=SERIE_CHOICES, default=1)
     email = models.EmailField(max_length = 200)
     senha = models.CharField(max_length = 30)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
