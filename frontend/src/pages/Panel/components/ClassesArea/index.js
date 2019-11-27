be65419c5b7535c4e259958b12b111a592a7e855
Guilherme Silva <guisilva.master@gmail.com>
2019-11-27 12:19:46 -0300

    Style Module List

--- frontend/src/pages/Panel/components/ClassesArea/index.js
+++ frontend/src/pages/Panel/components/ClassesArea/index.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react';
-import { List, Avatar } from 'antd';
+import { List, Icon } from 'antd';
 
-import { Container, Content } from './styles';
-import avatar from '../../../../assets/images/avatar.jpeg';
+import { Container, Content, NList } from './styles';
 
 export default class ClassesArea extends Component {
   state = {
-    data: [{}],
+    data: [
+      {
+        title: 'Módulo 1',
+        description: 'Lógica de Programação',
+      },
+      {
+        title: 'Módulo 2',
+        description: 'Noções Básicas de Programação',
+      },
+      {
+        title: 'Módulo 3',
+        description: 'Algoritmo',
+      },
+      {
+        title: 'Módulo 4',
+        description: 'Ética de Programação',
+      },
+    ],
   };
 
   componentDidMount() {
     this.fetchData();
   }
   fetchData = () => {
-    const response = {};
+    // const response = {};
     // this.setState({ data });
   };
   render() {
     const { data } = this.state;
     return (
       <Container>
-        <Avatar size={96} src={avatar} style={{ marginTop: '25px' }} />
-        {/* <Img src={avatar} /> */}
         <Content>
-          <List
+          <NList
             style={{ width: '80%' }}
             itemLayout="horizontal"
             dataSource={data}
             renderItem={item => (
               <List.Item>
+                <Icon type="left" />
                 <List.Item.Meta
                   title={item.title}
                   description={item.description}
--- frontend/src/pages/Panel/components/ClassesArea/styles.js
+++ frontend/src/pages/Panel/components/ClassesArea/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { List } from 'antd';
 
 export const Container = styled.div`
   display: flex;
@@ -8,9 +9,29 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  margin-top: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
+`;
+
+export const NList = styled(List)`
+  width: 100% !important;
+
+  .anticon {
+    margin: 0 10px;
+  }
+
+  .ant-list-item-meta-description {
+    font-style: italic;
+    color: rgba(0, 0, 0, 0.25);
+  }
+
+  .ant-list-item-meta-title {
+    font-weight: bold;
+  }
+
+  .ant-list-item:nth-child(2n + 2) {
+    background-color: #ededed;
+  }
 `;
